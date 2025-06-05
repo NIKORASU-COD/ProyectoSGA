91c2faaba36d3bd0ef2e79812fa9fd1e934dee35
EivorKinKest <jmqo2015@gmail.com>
2025-06-05 12:20:06 -0500

    cambios hechos acerca de la base de datos

---
+++ PROCESOS.sql
@@ -0,0 +1,108 @@
+
+#proceso de usuraio#
+USE `proyectosga`;
+DROP procedure IF EXISTS `insert_usuario`;
+
+DELIMITER $$
+USE `proyectosga`$$
+CREATE PROCEDURE `insert_usuario` (
+IN Num_Doc varchar(15),
+IN Nom1 varchar (18),
+IN Nom2 varchar (18),
+IN Ape1 varchar (20),
+IN Ape2 varchar (20),
+IN Dirección varchar (35),
+IN NumTel bigint,
+IN ID_Barrio smallint,
+IN ID_TipoDoc tinyint,
+IN ID_Rol tinyint
+
+)
+BEGIN
+INSERT INTO Usuarios (Num_Doc, Nom1, Nom2, Ape1, Ape2, Dirección, NumTel, ID_Barrio, ID_TipoDoc, ID_Rol)
+VALUES (Num_Doc, Nom1, Nom2, Ape1, Ape2, Dirección, NumTel, ID_Barrio, ID_TipoDoc, ID_Rol);
+END$$
+
+DELIMITER ;
+
+#proceso articulos#
+USE `proyectosga`;
+DROP procedure IF EXISTS `insert_articulos`;
+
+DELIMITER $$
+USE `proyectosga`$$
+CREATE PROCEDURE `insert_articulos` (
+IN ID_Articulo smallint,
+IN NomArti varchar(30),
+IN Genero varchar(15),
+IN Talla varchar(5),
+IN Color varchar(20),
+IN Precio mediumint,
+IN ID_Categoria tinyint
+
+)
+BEGIN
+INSERT INTO Articulos (ID_Articulo, NomArti, Genero, Talla, Color, Precio, ID_Categoria)
+VALUES (ID_Articulo, NomArti, Genero, Talla, Color, Precio, ID_Categoria);
+END$$
+
+DELIMITER ;
+
+#proceso factura#
+USE `proyectosga`;
+DROP procedure IF EXISTS `insert_facturas`;
+
+DELIMITER $$
+USE `proyectosga`$$
+CREATE PROCEDURE `insert_facturas` (
+IN ID_Orden smallint,
+IN FechaFac date,
+IN Saldo mediumint,
+IN ID_Alquiler smallint
+
+)
+BEGIN
+INSERT INTO Facturas ( ID_Orden, FechaFac, Saldo, ID_Alquiler)
+VALUES ( ID_Orden, FechaFac, Saldo, ID_Alquiler);
+END$$
+
+DELIMITER ;
+
+#proceso pago#
+USE `proyectosga`;
+DROP procedure IF EXISTS `insert_pagos`;
+
+DELIMITER $$
+USE `proyectosga`$$
+CREATE PROCEDURE `insert_pagos` (
+IN ID_Pago smallint,
+IN FechaUltiPago date,
+IN ValorAbono mediumint,
+IN ID_Orden smallint
+)
+BEGIN
+INSERT INTO pagos(ID_Pago,FechaUltiPago ,ValorAbono,ID_Orden)
+VALUE (ID_Pago,FechaUltiPago ,ValorAbono,ID_Orden);
+END$$
+
+DELIMITER ;
+
+#proceso alquiler#
+USE `proyectosga`;
+DROP procedure IF EXISTS `insert_alquiler`;
+
+DELIMITER $$
+USE `proyectosga`$$
+CREATE PROCEDURE `insert_alquiler` (
+ IN ID_Alquiler smallint,
+IN FechaRet date,
+IN FechaEnt date,
+IN Num_Doc varchar(15)
+)
+
+BEGIN
+INSERT INTO Alquiler (ID_Alquiler,FechaRet,FechaEnt,Num_Doc)
+VALUES (ID_Alquiler,FechaRet,FechaEnt,Num_Doc );
+END$$
+
+DELIMITER ;
